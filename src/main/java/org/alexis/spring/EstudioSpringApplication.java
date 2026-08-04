9987aaeabd8c3fda3eeba12625761041a4fd42dc
Alexis Jacob <ferjacobka@gmail.com>
2026-08-03 22:13:55 -0400

    chore: iniciando spring

---
+++ src/main/java/org/alexis/spring/EstudioSpringApplication.java
@@ -0,0 +1,13 @@
+package org.alexis.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudioSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstudioSpringApplication.class, args);
+    }
+
+}
