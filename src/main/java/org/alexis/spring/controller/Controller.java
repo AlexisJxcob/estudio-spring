8a0ea9011c0ede1219a3cd44c0805eca27d735c1
Alexis Jacob <ferjacobka@gmail.com>
2026-08-04 22:17:01 -0400

    chore: comentarios controller

--- src/main/java/org/alexis/spring/controller/Controller.java
+++ src/main/java/org/alexis/spring/controller/Controller.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+// Un controlador en Spring es, de forma muy sencilla, el recepcionista o el mesero de tu aplicación.
+
+// Es el componente encargado de recibir las peticiones que llegan desde internet (por ejemplo, desde una página web, un celular o Postman), procesarlas o pasarlas a quien corresponda, y devolver una respuesta.
 @RestController
 @RequestMapping("/apisaludos") // todos los endpoints piden esto adelante por si tenemos distintos controladores
 public class Controller {
