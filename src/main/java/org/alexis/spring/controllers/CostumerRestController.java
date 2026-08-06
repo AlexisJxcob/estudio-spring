f5f84fbf9ebee65dbe222518bae35a9e2cb68928
Alexis Jacob <ferjacobka@gmail.com>
2026-08-05 21:07:17 -0400

    chore: costumer api completa con list

--- src/main/java/org/alexis/spring/controllers/CostumerRestController.java
+++ src/main/java/org/alexis/spring/controllers/CostumerRestController.java
@@ -1,4 +1,25 @@
 package org.alexis.spring.controllers;
 
+import org.alexis.spring.domain.Costumer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class CostumerRestController {
+    private List<Costumer> costumers = new ArrayList<>(Arrays.asList(
+            new Costumer(1, "fernando", "alexis", "secreto123"),
+            new Costumer(2, "debbie", "amor", "secreto123"),
+            new Costumer(3, "abi", "chuela", "secreto123"),
+            new Costumer(4, "seporah", "avi", "secreto123")
+    ));
+
+    @GetMapping("/clientes")
+    public List<Costumer> getCostumers() {
+        return costumers;
+    }
+
 }
