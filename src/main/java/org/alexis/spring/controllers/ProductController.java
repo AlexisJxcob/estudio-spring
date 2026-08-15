797d4714cf2e545df8f1f5cb19bcbd6d28bb2032
Alexis Jacob <ferjacobka@gmail.com>
2026-08-15 11:16:31 -0400

    chore:services impl with prod controller get prod with http

--- src/main/java/org/alexis/spring/controllers/ProductController.java
+++ src/main/java/org/alexis/spring/controllers/ProductController.java
@@ -1,12 +1,23 @@
 package org.alexis.spring.controllers;
 
+import org.alexis.spring.domain.Product;
+import org.alexis.spring.services.ProductServiceImpl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/productos") // todos los endpoints piden esto adelante por si tenemos distintos controladores
 public class ProductController {
 
+    ProductServiceImpl productService = new ProductServiceImpl();
 
+    @RequestMapping
+    public ResponseEntity<List<Product>> getProducts() {
+        List<Product> products = productService.getProducts();
+        return ResponseEntity.ok(products);
+    }
 
 }
