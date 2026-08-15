d64ee61b1f6f2705a8e84991d7abb07475d399fb
Alexis Jacob <ferjacobka@gmail.com>
2026-08-15 16:28:29 -0400

    chore:archivo json con datos ahora a serializar

---
+++ src/main/java/org/alexis/spring/services/ProductosServiceJSONImpl.java
@@ -0,0 +1,21 @@
+package org.alexis.spring.services;
+
+import org.alexis.spring.domain.Product;
+
+import java.util.List;
+
+public class ProductosServiceJSONImpl implements ProductService{
+
+    @Override
+    public List<Product> getProducts() {
+        List<Product> products;
+
+        try {
+
+        }
+        catch (Exception e) {
+
+        }
+
+    }
+}
