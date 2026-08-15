ca9aa2c5e5c71bed55cca65746504b95db6c615b
Alexis Jacob <ferjacobka@gmail.com>
2026-08-15 11:11:18 -0400

    chore:services impl with prod

--- src/main/java/org/alexis/spring/services/ProductServiceImpl.java
+++ src/main/java/org/alexis/spring/services/ProductServiceImpl.java
@@ -1,7 +1,16 @@
 package org.alexis.spring.services;
 
+import org.alexis.spring.domain.Product;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ProductServiceImpl {
 
-
-
+List<Product> products = new ArrayList<>(Arrays.asList(
+        new Product(1, "Laptop", 1200.0, 10),
+        new Product(2, "Smartphone", 800.0, 20),
+        new Product(3, "Tablet", 400.0, 15),
+        new Product(4, "Monitor", 300.0, 5)));
 }
